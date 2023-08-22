0cfbd75f0c58fc1e3a130a07afe5c8ffadde93c4
Valenciatv001 <67506940+Valenciatv001@users.noreply.github.com>
2023-08-22 08:25:10 +0100

    DashBoard

---
+++ README.md
@@ -0,0 +1,42 @@
+# DashBoard
+
+![Project Logo](https://example.com/logo.png)
+
+AwesomeTaskTracker is a lightweight task management application that helps you stay organized and productive.
+
+## Features
+
+- Mark tasks as complete
+- Organize tasks into categories
+- Responsive and user-friendly interface
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/Valenciatv001/DashBoard.git`
+2. Navigate to the project directory: `cd DashBoard`
+3. Install dependencies: `npm install`
+4. Start the app: `npm start`
+
+## Usage
+
+1. Visit [http://127.0.0.1:5500/index.html](http://127.0.0.1:5500/index.html) in your web browser.
+2. Register or log in to your account.
+3. Start adding tasks and managing your to-do list.
+
+## Contributing
+
+We welcome contributions from the community! To contribute to AwesomeTaskTracker, follow these steps:
+
+1. Fork the repository.
+2. Create a new branch: `git checkout -b feature/new-feature`
+3. Make your changes and commit them: `git commit -m "Add new feature"`
+4. Push to your branch: `git push origin feature/new-feature`
+5. Open a pull request, describing your changes in detail.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+---
+
+Feel free to reach out to us at [irechukwuezekiel@gmail.com](mailto:irechukwuezekiel@gmail.com) if you have any questions or feedback!
---
+++ js/index.js
@@ -0,0 +1,80 @@
+const data = {
+  labels: [20, "", 25, "", 30, "", 35, "", 40, "", 60, "", 65],
+  datasets: [
+    {
+      label: "Employer: K 73,500",
+      backgroundColor: "#0800a3",
+      data: [5, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30],
+      barThickness: 12,
+      borderColor: "transparent",
+      borderWidth: 0,
+      fill: true,
+    },
+    {
+      label: "Employee: K 52,500",
+      backgroundColor: "#4935ff",
+      data: [8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32],
+      barThickness: 12,
+    },
+    {
+      label: "Total Interest: K 244,313",
+      backgroundColor: "#85afff",
+      data: [15, 22, 28, 35, 45, 50, 55, 60, 65, 70, 75, 80, 88],
+      barThickness: 12,
+    },
+  ],
+};
+const customXLabels = ["0$", "100$", "200$", "300$"];
+
+const config = {
+  type: "bar",
+  data: data,
+  options: {
+    layout: {
+      padding: {
+        top: 30,
+      },
+    },
+    responsive: false,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        stacked: true,
+        grid: {
+          display: false,
+          drawBorder: false,
+        },
+        border: {
+          display: false,
+        },
+      },
+      y: {
+        stacked: true,
+        grid: {
+          display: false,
+        },
+        border: {
+          display: false,
+        },
+
+        ticks: {
+          callback: (value, index, values) => {
+            return customXLabels[index] || value;
+          },
+          stepSize: 50,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+  },
+};
+
+const ctx = document.getElementById("myChart").getContext("2d");
+new Chart(ctx, config);
